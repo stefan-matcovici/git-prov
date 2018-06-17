3e5d8dd7f807781e1c79ac72897c433cfc14959a
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-06-17 11:41:43 +0300

    Changed provcontroller to match others signature and fixed # problem with namespace

--- src/main/java/ro/uaic/info/gitprov/controllers/ProvController.java
+++ src/main/java/ro/uaic/info/gitprov/controllers/ProvController.java
@@ -6,11 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ro.uaic.info.gitprov.services.GithubService;
 import ro.uaic.info.gitprov.services.ProvenanceService;
 
@@ -25,7 +21,7 @@ import static ro.uaic.info.gitprov.utils.ControllerUtils.getProvControllerProven
 /**
  * The Prov controller.
  */
-@Controller
+@RestController
 @RequestMapping(value = "/repos")
 public class ProvController {
     @Autowired
--- src/main/java/ro/uaic/info/gitprov/utils/ControllerUtils.java
+++ src/main/java/ro/uaic/info/gitprov/utils/ControllerUtils.java
@@ -8,10 +8,10 @@ import ro.uaic.info.gitprov.controllers.StoreController;
 public class ControllerUtils {
 
     public static String getProvControllerProvenanceNamespace(@PathVariable String owner, @PathVariable String name) {
-        return ControllerLinkBuilder.linkTo(ProvController.class).slash("owner").slash(owner).slash(name).toString();
+        return ControllerLinkBuilder.linkTo(ProvController.class).slash("owner").slash(owner).slash(name).toString() + '#';
     }
 
     public static String getStoreControllerProvenanceNamespace(@PathVariable String owner, @PathVariable String name) {
-        return ControllerLinkBuilder.linkTo(StoreController.class).slash("owner").slash(owner).slash(name).toString();
+        return ControllerLinkBuilder.linkTo(StoreController.class).slash("owner").slash(owner).slash(name).toString() + '#';
     }
 }
